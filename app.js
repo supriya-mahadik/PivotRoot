410993ce6fb282ce95219f4f4ab65275427152e4
supriya-mahadik <m66supriya@gmail.com>
2021-11-26 15:28:23 +0530

    Done data binding

--- app.js
+++ app.js
@@ -1,22 +1,102 @@
-//https://stackoverflow.com/questions/7861032/loop-and-get-key-value-pair-for-json-array-using-jquery
 $(document).ready(function () {
-    var student = '';
     // FETCHING DATA FROM JSON FILE
     $.getJSON("data.json", function (data) {
-        console.log("data::",data);
-        console.log("index::",data.length);
-        student += data;
-        
-        // ITERATING THROUGH OBJECTS
-        $(data).each(function(i, v) {
-            console.log("element::",i, v);
-            console.log("element::",v);
-        });
-       
+        getData(data)
     });
-    $('#PRoot').append('<div>'+ student +'</div>');
 });
 
+function getData(data){
+    // ITERATING THROUGH OBJECTS
+    $(data).each(function(i, v) {
+        if(v.monthly){
+            getMonthlyData(v.monthly)
+        }
+        if(v.yearly){
+            getYearlyData(v.yearly)
+        }
+    });
+    $('#yearly').on('click', function(){
+        getYearlyData(data[1].yearly);
+    })
+    $('#monthly').on('click',function(){
+        getMonthlyData(data[0].monthly);
+    })
+}
+
+function getYearlyData(data){
+    $('#boardsTab button').remove();
+    //setting default grade in option with all tabls and its data
+    if(data[0].grade){
+        $('#yGrades').val(data[0].grade);
+    }
+    $.each(data[0].boards, function (boardName, boardDataObj) {
+        $('#boardsTab').append($('<button/>', {
+            text: boardName,
+            id: 'btn_'+boardName,
+            click: () => displayBoardData(boardDataObj)
+        }));
+    })
+    $('#boardsTab button:first-child').click();
+    $('#yGrades option').remove();
+    $.each(data, function (i, item) {
+        $('#yGrades').append($('<option>', { 
+            value: item.grade,
+            text : item.grade 
+        }));
+    });
+        
+    $('#yGrades').change(function(){ 
+        var value = $(this).val();
+        data.forEach(item => {
+            if (item.grade === value) {
+                displayBoardsTab(item, value);
+            }
+        });
+        $('#boardsTab button:first-child').click();
+    });
+}
+// Yearly data functions start
+function displayBoardsTab(data, grade){
+    $('#boardsTab button').remove();
+    for (const boardName in data.boards) {
+        if (Object.hasOwnProperty.call(data.boards, boardName)) {
+            const boardObject = data.boards[boardName];
+            $('#boardsTab').append($('<button/>', {
+                text: boardName,
+                id: 'btn_'+boardName,
+                click: () => displayBoardData(boardObject)
+            }));
+        }
+    }
+}
+
+function displayBoardData(boardObject){
+    $('#totalSessions').text(boardObject.total_sessions);
+    $('#olPreAsmt').text(boardObject.online_pre_assignments);
+    $('#olPostAsnt').text(boardObject.online_post_assignments);
+    $('#olPractice').text(boardObject.online_assignments);
+    $('#olTests').text(boardObject.online_tests);
+    $('#careerSessions').text(boardObject.career_counselling_sessions);
+    if(Array.isArray(boardObject.syllabus)){
+        $.each(boardObject.syllabus[0], function (key, item) {
+            let itemList = item.split("!").map(el => el.trim());
+            $('#syllabus').append(`<div>${key}</div>`);
+            itemList.forEach(it => {
+                $('#syllabus').append(`<div>${it}</div>`);
+            });
+        });
+    }else{
+        let itemList = (boardObject.syllabus).split("!").map(el => el.trim());
+        itemList.forEach(it => {
+            $('#syllabus').append(`<div>${it}</div>`);
+        });
+    }
+   
+    $('#seats').text(boardObject.seats);
+    $('#discount').text(boardObject.discount);
+    $('#price').text(boardObject.price);
+    $('#perClassPrice').text(boardObject.per_class_price);
+}
 
 function openCourse(evt, courseName) {
     var i, tabcontent, tablinks;
@@ -30,4 +110,76 @@ function openCourse(evt, courseName) {
     }
     document.getElementById(courseName).style.display = "block";
     evt.currentTarget.className += " active";
-  }
+}
+
+// Yearly data functions end
+
+function getMonthlyData(data){
+    $('#mboardsTab button').remove();
+    if(data[0].grade){
+         $('#mGrades').val(data[0].grade);
+     }
+    $.each(data[0].boards, function (boardName, boardDataObj) {
+        $('#mboardsTab').append($('<button/>', {
+            text: boardName,
+            id: 'btn_'+boardName,
+            click: () => displayMonthlyBoardData(boardDataObj)
+        }));
+    })
+    $('#mboardsTab button:first-child').click();
+    $('#mGrades option').remove();
+     $.each(data, function (i, item) {
+         $('#mGrades').append($('<option>', { 
+             value: item.grade,
+             text : item.grade 
+         }));
+     });
+     $('#mGrades').change(function(){ 
+         var value = $(this).val();
+         data.forEach(item => {
+             if (item.grade === value) {
+                 displayMonthlyBoardsTab(item, value);
+             }
+         });
+         $('#mboardsTab button:first-child').click();
+     });
+     
+ }
+
+ function displayMonthlyBoardsTab(data, grade){
+    $('#mboardsTab button').remove();
+    for (const boardName in data.boards) {
+        if (Object.hasOwnProperty.call(data.boards, boardName)) {
+            const boardObject = data.boards[boardName];
+            $('#mboardsTab').append($('<button/>', {
+                text: boardName,
+                id: 'btn_'+boardName,
+                click: () => displayMonthlyBoardData(boardObject)
+            }));
+        }
+    }
+}
+
+function displayMonthlyBoardData(boardObject){
+    $('#radioWrapper').html('');
+    $.each(boardObject, function (key, item) {
+            $('#radioWrapper')
+                .append(`<input type="radio" id="${key}" name="contact" value="${key}">
+                <label for="${key}">
+                    <div>
+                        <div class="valid">${item.valid}</div>
+                        <div class="refund">${item.refund}</div>
+                    </div>
+                    <div>
+                        <div class="price">${item.price}</div>
+                        <div class="discount">${item.discount}</div>
+                    </div>
+                    <div>
+                        <div class="perClassPrice">${item.per_class_price}</div>
+                        <div class="total_sessions">${item.total_sessions}</div>
+                    </div>
+                </label>`);
+       
+        
+    });
+}
